7377292370c85eeb5866302f753958aac8ac5930
Torea Patissier <p.torea@icloud.com>
2023-03-12 17:37:23 +0100

    WIP: reveal animation aPropos

--- src/components/sections/APropos/ModeInterventions.js
+++ src/components/sections/APropos/ModeInterventions.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import Image from '../../elements/Image';
 
 const ModeInterventions = () => {
+
+  const elementsRef = useRef([]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries =>{
+      entries.forEach(entry => {
+        if(entry.isIntersecting){
+          entry.target.classList.add('is-revealed')
+        }
+      })
+    })
+
+    elementsRef.current.forEach(element =>{
+      observer.observe(element);
+    })
+
+    return () => observer.disconnect();
+  }, [])
+
 
   return (
      <div id="modes_intervention" className="modeInterventioParent pb-32">
@@ -17,17 +36,17 @@ const ModeInterventions = () => {
                 src={require('../../../assets/images/collab.svg')}
                 alt="ModeInterventions"
              />
-           <div className="hLeft">
+           <div className="hLeft" ref={el => elementsRef.current[0] = el}>
              <h5 className="titleNotreVision">Régie :</h5>
              <p className="text-sm text-justify m-16">Assistance technique unitaire ou groupée. Mise à disposition d'un
                ou plusieurs collaborateurs possédant l'expertise nécessaire.</p>
            </div>
-           <div className="cLeft">
+           <div className="cLeft" ref={el => elementsRef.current[1] = el}>
              <h5 className="titleNotreVision">Création de centre de service :</h5>
              <p className="text-sm text-justify m-16"> Maintenir et faire évoluer une solution métier pour
                l'entreprise.</p>
            </div>
-           <div className="bLeft">
+           <div className="bLeft" ref={el => elementsRef.current[2] = el}>
              <h5 className="titleNotreVision">Forfait :</h5>
              <p className="text-sm text-justify m-16">Une équipe dédiée et engagée pour vous accompagner de manière
                personnalisée.</p>
--- src/components/sections/APropos/NotreApproche.js
+++ src/components/sections/APropos/NotreApproche.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import Image from "../../elements/Image";
 
 const NotreApproche = () => {
+  const elementsRef = useRef([]);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('is-revealed');
+        }
+      });
+    });
+
+    elementsRef.current.forEach(element => {
+      observer.observe(element);
+    });
+
+    return () => observer.disconnect();
+  }, []);
 
   return (
      <div id="notre_approche" className="notreApprocheParent pb-32">
@@ -17,18 +34,18 @@ const NotreApproche = () => {
               src={require('../../../assets/images/intervention.svg')}
               alt="ModeInterventions"
            />
-           <div className="hRight">
+           <div className="hRight" ref={el => elementsRef.current[0] = el}>
              <h5 className="titleNotreVision">Rise Consulting à votre écoute :</h5>
              <p className="text-sm text-justify m-16">Entretiens avec les acteurs clés, prise de connaissance de votre
                environnement, de vos spécificités et de vos enjeux.</p>
            </div>
-           <div className="cRight">
+           <div className="cRight" ref={el => elementsRef.current[1] = el}>
              <h5 className="titleNotreVision">Elaboration du besoin :</h5>
              <p className="text-sm text-justify m-16">Chiffrage et cadrage du projet, mise à disposition de nos talents
                afin de créer durablement la valeur nécessaire à votre réussite, ainsi que l’identification des objectifs
                clés.</p>
            </div>
-           <div className="bRight">
+           <div className="bRight" ref={el => elementsRef.current[2] = el}>
              <h5 className="titleNotreVision">Réalisation de votre projet :</h5>
              <p className="text-sm text-justify m-16">Mise en place et déploiement d’une solution personnalisée et
                adaptée à votre environnement, en assistance technique ou projet à engagement.</p>
